64199905f6fc7e57529cdd2cce33cbe848e94682
Nicole-Hua <nicole.hua111@gmail.com>
2021-06-11 17:17:49 +1000

    Week 2 Exercises In Progress

--- set2/exercise1.py
+++ set2/exercise1.py
@@ -11,18 +11,25 @@ import platform
 # I think this will print "hello! Let's get started" by calling the print function.
 print("hello! Let's get started")  # it printed "hello! Let's get started"
 
-some_words = ['what', 'does', 'this', 'line', 'do', '?']
+# I think it will declare a variable called some_words and it'll put a list of strings into it.
+some_words = ["what", "does", "this", "line", "do", "?"]
 
+# I think it will print individual words from some_words list in order.
 for word in some_words:
-    print(word)
+    print(word)  # Prints every word on a new line in order till exhausted.
 
-for x in some_words:
+# I think it will print the same as above just the variable is now called x
+for x in some_words:  # it printed the same results as above
     print(x)
 
-print(some_words)
+# It will print the list of the variable some_words
+print(some_words)  # Prints the list of some_words
 
-if len(some_words) > 3:
-    print('some_words contains more than 3 words')
+if len(some_words) > 3:  # prints some_words contains more than 3 words
+    print(
+        "some_words contains more than 3 words"
+    )  # printed some_words contains more than 3 words.
+
 
 def usefulFunction():
     """
@@ -30,6 +37,7 @@ def usefulFunction():
     what the line below does:
     https://docs.python.org/3/library/platform.html#platform.uname
     """
-    print(platform.uname())
+    print(platform.uname())  # I think this will print the systems attributes
+
 
 usefulFunction()
--- set2/readme.md
+++ set2/readme.md
@@ -1,1 +1,15 @@
-TODO: Reflect on what you learned this week and what is still unclear.
+Updated official email from zid to name
+Updated the tests for sets
+python ../course/set2/tests.py
+() means that it is a string
+.upper or .lower to make the string either in upper or lowercase
+str() function converts the specified value into a string
+Plus sign (+) can add more to the string adding a number or character after the string (e.g. a_string.upper() + "!" will make the string upper case and with an !)
+This " " creates a space between letters/ characters
+To debug the syntax errors run the script and it will show you were the errors are in the terminal to fix
+This == means equal
+This len means length
+elif means else if
+.append() adds to the end of a list
+Watch the indentation if there is a syntax error that isn't obvious
+For Loop twice to create a list within a list like a branching system
