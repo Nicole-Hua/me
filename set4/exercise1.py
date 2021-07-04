135d148434e7b9a60ef7e91ef7c6ba3d963dfad6
Nicole-Hua <nicole.hua111@gmail.com>
2021-07-04 16:23:13 +1000

    Updated Set 4 + Readme

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -97,24 +97,6 @@ def wordy_pyramid():
 
     return pyramid
 
-    # alternative method using refractor - easier to read visually without having to have double up on the same coding
-    # pyramid = []
-    #
-    # for i in range(3,21,2): (first half > values in the range are the start, stop, step to create the pyramid)
-    #   word = get_a_word(i)
-    #   pyramid.append(word)
-    #
-    # for i in range(20,3,-2): (second half of pyramid)
-    #   word = get_a_word(i)
-    #   pyramid.append(word)
-    #
-    # def get_a_word(i):
-    #   url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={}"
-    #   response = requests.get(url.format(i))
-    #   word = response.text
-    #
-    #   return word
-
 
 def pokedex(low=1, high=5):
     """Return the name, height and weight of the tallest pokemon in the range low to high.
--- set4/readme.md
+++ set4/readme.md
@@ -1,1 +1,21 @@
-TODO: Reflect on what you learned this week and what is still unclear.
+Exercise 1 - wordy pyramid
+(alternative method using refractor - easier to read visually without having to have double up on the same coding)
+
+pyramid = []
+
+for i in range(3,21,2): (first half > values in the range are the start, stop, step to create the pyramid)
+word = get_a_word(i)
+pyramid.append(word)
+
+for i in range(20,3,-2): (second half of pyramid)
+word = get_a_word(i)
+pyramid.append(word)
+
+def get_a_word(i):
+url = "https://us-central1-waldenpondpress.cloudfunctions.net/give_me_a_word?wordlength={}"
+response = requests.get(url.format(i))
+word = response.text
+
+return word
+
+(Value at end of URL determines the word length - needs to be formatted to change its value in the loop)
